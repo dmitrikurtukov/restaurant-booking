0a51088dc0364625d2b00b487d0fc2ce32008971
dmitrikurtukov <kurtukov9@gmail.com>
2026-02-21 12:17:55 +0200

    feat: init spring boot with liquibase and postgres

---
+++ backend/src/test/java/ee/cgi/restaurant/RestaurantBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.cgi.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
